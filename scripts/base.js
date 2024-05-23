8df54a86f6b6fd2d4b627a6ed9474e456351e4ca
Richard Noh <richard.jun.noh@gmail.com>
2024-05-23 18:30:25 -0500

    Add fully functional projects tab

--- scripts/base.js
+++ scripts/base.js
@@ -1,17 +1,7 @@
-// menu interaction
+
 document.addEventListener('DOMContentLoaded', function() {
-    const sections = {
-        'home-content': document.getElementById('home-content'),
-        'projects-content': document.getElementById('projects-content'),
-    };
-
-    document.getElementById('logo').addEventListener('click', () => showContent('home-content'));
-    document.getElementById('projects-button').addEventListener('click', () => showContent('projects-content'));
-
-    function showContent(sectionId) {
-        Object.values(sections).forEach(section => section.style.display = 'none');
-        sections[sectionId].style.display = 'block';
-    }
-
-    showContent('home-content');
+    setupMenu();
+    projectsFactory = new ProjectsList("projects-list")
+    // initProjects();
+    // initProjectNavButtons();
 });
---
+++ scripts/nav.js
@@ -0,0 +1,17 @@
+function setupMenu()
+{
+    const sections = {
+        'home-content': document.getElementById('home-content'),
+        'projects-content': document.getElementById('projects-content'),
+    };
+
+    document.getElementById('logo').addEventListener('click', () => showContent('home-content'));
+    document.getElementById('projects-button').addEventListener('click', () => showContent('projects-content'));
+
+    function showContent(sectionId) {
+        Object.values(sections).forEach(section => section.style.display = 'none');
+        sections[sectionId].style.display = 'block';
+    }
+
+    showContent('home-content');
+}
